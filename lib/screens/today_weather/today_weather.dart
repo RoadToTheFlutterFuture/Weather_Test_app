760616640893db04e26a8364b6abddf22680ab1d
Ilya Safyan <ilyasafyan@Ilya.local>
2022-01-31 19:12:21 +0300

    [step2_presentationLayer]: added Scafold with bottom navigation bar to today_weather screen

--- lib/screens/today_weather/today_weather.dart
+++ lib/screens/today_weather/today_weather.dart
@@ -1,15 +1,32 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_test_app/widgets/weather_bottomNavigationBar/weather_bottomNavigationBar.dart';
 
 class TodayWeather extends StatelessWidget {
-  const TodayWeather({Key? key}) : super(key: key);
+    const TodayWeather({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Text('page_title.today_weather'.tr()),
-        ),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Today'), //Todo: translate it | Today
+            ),
+            bottomNavigationBar: WeatherBottomNavigationBar(),
+            body: Column(
+                children: [
+                    Flexible(
+                        flex: 3,
+                        child: Container(color: Colors.green,)
+                    ),
+                    Flexible(
+                        flex: 2,
+                        child: Container(color: Colors.red,)
+                    ),
+                    Flexible(
+                        flex: 1,
+                        child: Container(color: Colors.blue,)
+                    ),
+                ],
+            ),
+        );
+    }
 }
